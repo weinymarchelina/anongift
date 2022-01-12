acebb36c0c8302f1e9e720c6fef8a3200606e8e7
weinymarchelina <weinymarchelina31@gmail.com>
2022-01-12 10:56:04 +0800

    update favicon

--- components/Layout.js
+++ components/Layout.js
@@ -1,8 +1,17 @@
+import Head from "next/head";
 import Navbar from "./Navbar";
 
 const Layout = ({ children }) => {
   return (
     <div className="content">
+      <Head>
+        <title>Thanksage</title>
+        <meta
+          name="description"
+          content="Send your thankful message to anyone secretly!"
+        />
+        <link rel="icon" href="/mini.png" />
+      </Head>
       <Navbar />
       {children}
     </div>
